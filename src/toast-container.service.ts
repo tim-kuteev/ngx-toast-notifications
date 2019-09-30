4d463433b0628fcf837fe13b516ff465b7c1ef60
Tim <tim.kuteev.github@gmail.com>
2019-09-30 18:32:07 +0100

    using angular renderer for dom manipulations

--- src/toast-container.service.ts
+++ src/toast-container.service.ts
@@ -6,28 +6,30 @@ import {
   Inject,
   Injectable,
   Injector,
-  OnDestroy
+  OnDestroy,
+  Renderer2,
+  RendererFactory2
 } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { ToastContainerComponent } from './toast-container/toast-container.component';
 
+const TOAST_CONTAINER_CLASS_NAME = 'toast-container';
+
 @Injectable()
 export class ToastContainerService implements OnDestroy {
 
+  private _renderer: Renderer2;
   private _containerElement: HTMLElement;
   private _componentRef: ComponentRef<ToastContainerComponent>;
 
   constructor(
+    rendererFactory: RendererFactory2,
     @Inject(DOCUMENT) private _document: any,
     private _factoryResolver: ComponentFactoryResolver,
     private _appRef: ApplicationRef,
     private _injector: Injector,
   ) {
-  }
-
-  ngOnDestroy() {
-    this._detach();
-    this._destroyContainer();
+    this._renderer = rendererFactory.createRenderer(null, null);
   }
 
   get ref(): ComponentRef<ToastContainerComponent> {
@@ -37,6 +39,20 @@ export class ToastContainerService implements OnDestroy {
     return this._componentRef;
   }
 
+  private get containerElement(): HTMLElement {
+    if (!this._containerElement) {
+      this._containerElement = this._renderer.createElement('div');
+      this._renderer.addClass(this._containerElement, TOAST_CONTAINER_CLASS_NAME);
+      this._renderer.appendChild(this._document.body, this._containerElement);
+    }
+    return this._containerElement;
+  }
+
+  ngOnDestroy() {
+    this._detach();
+    this._destroyContainer();
+  }
+
   private _attach() {
     this._detach();
     const componentFactory = this._factoryResolver.resolveComponentFactory(ToastContainerComponent);
@@ -44,7 +60,7 @@ export class ToastContainerService implements OnDestroy {
     const hostView = this._componentRef.hostView as EmbeddedViewRef<any>;
     this._appRef.attachView(hostView);
     const rootNode = hostView.rootNodes[0] as HTMLElement;
-    this._getContainerElement().appendChild(rootNode);
+    this._renderer.appendChild(this.containerElement, rootNode);
   }
 
   private _detach() {
@@ -55,18 +71,9 @@ export class ToastContainerService implements OnDestroy {
     }
   }
 
-  private _getContainerElement(): HTMLElement {
-    if (!this._containerElement) {
-      this._containerElement = this._document.createElement('div');
-      this._containerElement.classList.add('toast-container');
-      this._document.body.appendChild(this._containerElement);
-    }
-    return this._containerElement;
-  }
-
   private _destroyContainer() {
     if (this._containerElement && this._containerElement.parentNode) {
-      this._containerElement.parentNode.removeChild(this._containerElement);
+      this._renderer.removeChild(this._containerElement.parentNode, this._containerElement);
       this._containerElement = null;
     }
   }
