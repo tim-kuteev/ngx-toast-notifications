d45ab44eacb1d3c99a9d3b935dda065c48bf9cb8
Tim <tim.kuteev.github@gmail.com>
2019-11-20 11:58:43 +0000

    using relative resource urls instead of inlining

--- lib/src/toast-content/basic-toast-content.component.ts
+++ lib/src/toast-content/basic-toast-content.component.ts
@@ -2,8 +2,8 @@ import { Component, Input } from '@angular/core';
 import { Toast } from '../toast';
 
 @Component({
-  template: '<div class="close-button" (click)="toast.close()">&times;</div><div class="content-body"><div *ngIf="toast.caption" class="title">{{toast.caption}}</div><div>{{toast.text}}</div></div>',
-  styles: [':host{display:block;position:relative}.close-button{border:0;background:none;position:absolute;font-size:1.6rem;top:.5rem;right:.8rem;line-height:.6;font-weight:bold;opacity:.3;color:inherit;cursor:pointer}@media(min-width: 576px){.close-button{font-size:1rem;top:.3rem;right:.4rem}}.close-button:hover{opacity:1}.content-body{padding:.5rem 2.2rem .5rem .5rem;font-size:.85rem;min-height:1rem}@media(min-width: 576px){.content-body{padding-right:1rem}}.content-body .title{font-weight:bold}'],
+  templateUrl: './basic-toast-content.component.html',
+  styleUrls: ['./basic-toast-content.component.scss'],
 })
 export class BasicToastContentComponent {
   @Input() toast: Toast;
