0a66bcd817ed8d67156fd00b56b0703849c5b710
Tim <tim.kuteev.github@gmail.com>
2020-05-05 00:45:30 +0100

    updating versions of packagr and controlling angular application

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display title text', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('ngx-toast-notifications app is running!');
+    expect(page.getTitleText()).toEqual('Angular Toast Notifications');
   });
 
   afterEach(async () => {
---
+++ karma.conf.js
@@ -0,0 +1,35 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+module.exports = {
+  basePath: '',
+  frameworks: ['jasmine', '@angular-devkit/build-angular'],
+  plugins: [
+    require('karma-jasmine'),
+    require('karma-chrome-launcher'),
+    require('karma-jasmine-html-reporter'),
+    require('karma-coverage-istanbul-reporter'),
+    require('@angular-devkit/build-angular/plugins/karma'),
+  ],
+  client: {
+    clearContext: false
+  },
+  coverageIstanbulReporter: {
+    dir: require('path').join(__dirname, 'coverage'),
+    reports: ['html', 'lcovonly', 'text-summary'],
+    fixWebpackSourcePaths: true,
+  },
+  reporters: ['progress', 'kjhtml'],
+  port: 9876,
+  colors: true,
+  autoWatch: true,
+  browsers: ['Chrome'],
+  customLaunchers: {
+    headless: {
+      base: 'ChromeHeadless',
+      flags: ['--no-sandbox'],
+    },
+  },
+  singleRun: false,
+  restartOnFileChange: true
+};
